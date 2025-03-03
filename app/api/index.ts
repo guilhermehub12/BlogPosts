8ba450d156d18f746c774cf8f3a16f6e6f38b59d
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-03 11:41:22 -0300

    Adds theme color and hook support
    Removes unused imports from API index
    Introduces color constants for light and dark modes
    Adds hooks for using color scheme and theme colors

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,14 +1,10 @@
-import apiClient from './config';
 import { ENDPOINTS } from './endpoints';
-import * as Types from './types';
 import postsService from './services/posts.service';
 import usersService from './services/users.service';
 import commentsService from './services/comments.service';
 
 export {
-  apiClient,
   ENDPOINTS,
-  Types,
   postsService,
   usersService,
   commentsService
